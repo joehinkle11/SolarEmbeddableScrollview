8a96141b4634415eb36bec1a8c527d7b0dc73e90
Joseph Hinkle <joehinkle11@gmail.com>
2019-06-16 01:35:22 -0400

    initial commit

---
+++ lua/main.lua
@@ -0,0 +1,19 @@
+-- 
+-- Abstract: embeddablescrollview Library Plugin Test Project
+-- 
+-- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
+-- Copyright (C) 2015 Corona Labs Inc. All Rights Reserved.
+--
+------------------------------------------------------------
+
+-- Load plugin library
+local embeddablescrollview = require "plugin.embeddablescrollview"
+
+-------------------------------------------------------------------------------
+-- BEGIN (Insert your sample test starting here)
+-------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+-- END
+-------------------------------------------------------------------------------
---
+++ lua/plugin/embeddablescrollview.lua
@@ -0,0 +1,15 @@
+local Library = require "CoronaLibrary"
+
+-- Create library
+local lib = Library:new{ name='embeddablescrollview', publisherId='io.joehinkle.embeddablescrollview' }
+
+-------------------------------------------------------------------------------
+-- BEGIN (Insert your implementation starting here)
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- END
+-------------------------------------------------------------------------------
+
+-- Return library instance
+return lib
